6e25f5818415d918b31c7f1f70edce5e1ab3363d
agencik007 <agencik007@gmail.com>
2022-05-11 18:11:59 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import 'express-async-errors';
+
+const app = express();
+
+app.use(cors({
+    origin: 'https://localhost:3000',
+}));
+app.use(express.json());
+
+app.listen(3001, '0.0.0.0', () => {
+    console.log(`Server listening on port http://localhost:3001`);
+});
