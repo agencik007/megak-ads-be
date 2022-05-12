4d52252f5eb80a457fa3c36c8fbf1729651200dc
agencik007 <82754234+agencik007@users.noreply.github.com>
2022-05-12 19:58:26 +0200

    Delete db.ts

--- utils/db.ts
+++
@@ -1,9 +0,0 @@
-import { createPool } from "mysql2";
-
-export const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'megak-etap7',
-    namedPlaceholders: true,
-    decimalNumbers: true,
-});
