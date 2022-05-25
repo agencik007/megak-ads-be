c084a4e42d315da7c55a86ec0a38698552da02af
agencik007 <agencik007@gmail.com>
2022-05-25 18:41:49 +0200

    Added prefix to all URLs.

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {Router} from "express";
 import cors from "cors";
 import 'express-async-errors';
 import {handleError, ValidationError} from "./utils/errors";
@@ -18,7 +18,11 @@ app.use(rateLimit({
     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 }));
 
-app.use('/ad', adRouter);
+const router = Router();
+
+router.use('/ad', adRouter);
+
+app.use('/api', router);
 
 app.use(handleError);
 
