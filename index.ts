de7a83a043d1c55d0a0f5c4aa57df26526e74136
agencik007 <agencik007@gmail.com>
2022-05-25 16:27:00 +0200

    Changed cors url address for correct.

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ import {adRouter} from "./routes/ad.router";
 const app = express();
 
 app.use(cors({
-    origin: 'https://localhost:3000',
+    origin: 'http://localhost:3000',
 }));
 
 app.use(express.json());
